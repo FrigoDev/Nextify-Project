79503bdbe747c05b5cd8878b7e133ce38793d465
FrigoAlex <alejo012903@hotmail.com>
2023-04-20 20:14:08 -0500

    🔍 Add metadata and icon into the head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Nextify is a NextJS project that works with TypeScript, React, Spotify Web API and TailwindCSS" />
+        <meta name="author" content="Alejandro Roman" />
+        <meta name="keywords" content="NextJS, TypeScript, React, Spotify, TailwindCSS" />
+        <link rel="icon" href="/SpotifyIcon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
