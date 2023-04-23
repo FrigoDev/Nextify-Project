1b33ff4c5a349c2e3c5636346f60fdaa1d99ebab
FrigoAlex <alejo012903@hotmail.com>
2023-04-22 23:34:26 -0500

    🏷️ Add tinterfaces for sessions and JWT

---
+++ types.d.ts
@@ -0,0 +1,20 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+import { type DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: DefaultSession["user"];
+    accessToken: string;
+    refreshToken: string;
+    username: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT extends Record<string, unknown>, DefaultJWT {
+    accessToken?: string;
+    refreshToken?: string;
+    username?: string;
+  }
+}
