a3ee33bfa4d5fafde585705c8f4816def97eb776
FrigoAlex <alejo012903@hotmail.com>
2023-04-29 22:12:04 -0500

    ✨ Update  _app pages to have redux integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,32 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+import type { AppProps } from "next/app";
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
+import Loading from "@/components/Loading";
+import MainLayout from "@/components/MainLayout";
+import { store, persistor } from "@/store/store";
+
+const App = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<{ session: Session }>) => {
+  return (
+    <SessionProvider session={session}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Loading>
+            <MainLayout>
+              <Component {...pageProps} />
+            </MainLayout>
+          </Loading>
+        </PersistGate>
+      </Provider>
+    </SessionProvider>
+  );
+};
+
+export default App;
