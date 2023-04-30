a7034bbed3b13864ab10398079f5dbf47c3507c9
FrigoAlex <alejo012903@hotmail.com>
2023-04-29 21:50:59 -0500

    🏷️ Add constants enums

---
+++ src/constants/index.ts
@@ -0,0 +1,19 @@
+export enum Pages {
+  HOME = "/",
+  LOGIN = "/login",
+  REGISTER = "/register",
+  PROFILE = "/profile",
+  RELEASES = "/releases",
+  FEATURED_PLAYLIST = "/featured-playlists",
+  CATEGORIES = "/categories",
+  PLAYLIST="/playlists",
+  ARTIST="/artist", 
+  ALBUM="/album",
+  SEARCH="/search",
+  TRACKS="/tracks",
+}
+
+export enum Assets {
+  LOGO = "/assets/images/SpotifyLogo.png",
+  DEFAULT_IMAGE = "/assets/images/SpotifyDefaultImage.png",
+}
