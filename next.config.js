21385a12026a97312e07d21b4df5e7801cb6f7b8
FrigoAlex <alejo012903@hotmail.com>
2023-04-29 21:41:40 -0500

    🔧 Add next config to support different domains

--- next.config.js
+++ next.config.js
@@ -2,9 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["i.scdn.co"]
-  }
+    domains: [
+      "dailymix-images.scdn.co",
+      "daily-mix.scdn.co",
+      "lineup-images.scdn.co",
+      "mosaic.scdn.co",
+      "i.scdn.co",
+      "t.scdn.co",
+      "platform-lookaside.fbsbx.com",
+      "platform-lookaside.fbsbx.com",
+    ],
+  },
 };
-
 
 module.exports = nextConfig;
