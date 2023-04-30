544fc8b34435b2033efe04b63273a927c7ad3f51
FrigoAlex <alejo012903@hotmail.com>
2023-04-29 22:09:12 -0500

    ✨ Add content into index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,129 @@
-const index = () => {
+import { GetServerSideProps } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import { getToken } from "next-auth/jwt";
+import { AiOutlineSetting } from "react-icons/ai";
+
+import Card from "@/components/Card";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import SectionDivider from "@/components/SectionDivider";
+import { Pages, Assets } from "@/constants/index";
+import spotifyApi from "@/lib/spotifyWebApi";
+import welcomeMessage from "@/utils/welcomeMessage";
+
+interface indexProps {
+  topTracks: SpotifyApi.PagingObject<SpotifyApi.TrackObjectFull>;
+  releases: SpotifyApi.PagingObject<SpotifyApi.AlbumObjectSimplified>;
+  featuredPlaylists: SpotifyApi.PagingObject<SpotifyApi.PlaylistObjectSimplified>;
+  categories: SpotifyApi.PagingObject<SpotifyApi.CategoryObject>;
+}
+
+const Index = ({
+  topTracks,
+  releases,
+  featuredPlaylists,
+  categories,
+}: indexProps) => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex-grow items-center h-screen overflow-y-scroll scrollbar-hide pb-4">
+      <Header>
+        <div className="flex flex-col mt-10 max-[550px]:mt-4">
+          <div className="flex flex-row justify-between mb-4 space-x-4">
+            <h1 className="my-auto font-bold text-4xl max-[470px]:text-3xl">
+              {welcomeMessage()}
+            </h1>
+            <AiOutlineSetting className="my-auto text-4xl max-[470px]:text-3xl hidden max-[550px]:block cursor-pointer hover:" />
+          </div>
+          <div className="grid grid-cols-1 lg:grid-cols-3 sm:grid-cols-2 grid-flow-row md:gap-4 gap-2">
+            {topTracks.items.map((track) => (
+              <Link href={`/tracks/${track.id}`} key={track.id}>
+                <div className="flex flex-row m-2 cursor-pointer transition duration-500 ease-in-out bg-white bg-opacity-10 hover:bg-opacity-25 rounded-lg">
+                  <Image
+                    src={track?.album?.images[0]?.url ?? Assets.DEFAULT_IMAGE}
+                    width={80}
+                    height={80}
+                    alt={track.name}
+                    className="rounded-lg drop-shadow-xl"
+                  />
+                  <p className="text-md my-auto ml-4 break-words line-clamp-2">
+                    {track.name}
+                  </p>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </Header>
+      <SectionDivider name="New Releases" url={Pages.RELEASES}>
+        {releases.items.map((release) => (
+          <Card
+            key={release.id}
+            image={release?.images[0]?.url ?? Assets.DEFAULT_IMAGE}
+            title={release.name}
+            description={release.artists[0].name}
+            link={`/album/${release.id}`}
+          />
+        ))}
+      </SectionDivider>
+      <SectionDivider name="Featured Playlists" url={Pages.FEATURED_PLAYLIST}>
+        {featuredPlaylists.items.map((playlist) => (
+          <Card
+            key={playlist.id}
+            image={playlist?.images[0]?.url ?? Assets.DEFAULT_IMAGE}
+            title={playlist.name}
+            description={playlist?.description ?? ""}
+            link={`/playlist/${playlist.id}`}
+          />
+        ))}
+      </SectionDivider>
+      <SectionDivider name="Categories" url={Pages.CATEGORIES}>
+        {categories.items.map((category) => (
+          <Card
+            key={category.id}
+            image={category?.icons[0]?.url ?? Assets.DEFAULT_IMAGE}
+            title={category.name}
+            description={""}
+            link={`/category/${category.id}`}
+          />
+        ))}
+      </SectionDivider>
+      <Footer />
     </div>
   );
 };
-export default index;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const token = await getToken({
+    req: context.req,
+    secret: process.env.SECRET,
+  });
+  if (!token || !token.accessToken) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+
+  await spotifyApi.setAccessToken(token.accessToken);
+
+  const [topTracks, newReleasesData, featuredPlaylistsData, categories] =
+    await Promise.all([
+      spotifyApi.getMyTopTracks({ limit: 6, time_range: "short_term" }),
+      spotifyApi.getNewReleases({ limit: 10, country: "CO" }),
+      spotifyApi.getFeaturedPlaylists({ limit: 10, country: "CO" }),
+      spotifyApi.getCategories({ limit: 10, country: "CO" }),
+    ]);
+  return {
+    props: {
+      topTracks: topTracks.body,
+      releases: newReleasesData.body.albums,
+      featuredPlaylists: featuredPlaylistsData.body.playlists,
+      categories: categories.body.categories,
+    },
+  };
+};
+
+export default Index;
