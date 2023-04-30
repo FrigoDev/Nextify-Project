8362ddd7d37defd546e644988756c02addd20081
FrigoAlex <alejo012903@hotmail.com>
2023-04-29 22:07:55 -0500

    ✨ Add Main Layout component

---
+++ src/components/MainLayout/index.tsx
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+import Sidebar from "../Sidebar";
+
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const router = useRouter();
+  if (router.pathname === "/login") return <>{children}</>;
+  return (
+    <div className="bg-[#121212] h-screen overflow-hidden">
+      <Head>
+        <title>Nextify</title>
+      </Head>
+      <div className="flex">
+        <Sidebar />
+        {children}
+      </div>
+      <div>{/*Player*/}</div>
+    </div>
+  );
+}
