cb66fe0210fa3ec9ef931adde3f2ff1a4521f5e6
FrigoAlex <alejo012903@hotmail.com>
2023-04-29 22:00:42 -0500

    ✨ Create welcomeMessage utility

---
+++ src/utils/welcomeMessage.ts
@@ -0,0 +1,22 @@
+type Greeting = "Good morning" | "Good afternoon" | "Good evening" | "Good night";
+
+function getGreeting(): Greeting {
+  const hour = new Date().getHours();
+  const greetingsMap: [number, Greeting][] = [
+    [12, "Good morning"],
+    [18, "Good afternoon"],
+    [21, "Good evening"],
+  ];
+
+  const greeting =
+    greetingsMap.find(([time]) => hour < time)?.[1] || "Good night";
+
+  return greeting;
+}
+
+function welcomeMessage() {
+  const greeting = getGreeting();
+  return greeting;
+}
+
+export default welcomeMessage;
