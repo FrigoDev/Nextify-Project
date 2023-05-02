d510682ce6a9e5135a2dc9453b478613fa626c95
FrigoAlex <alejo012903@hotmail.com>
2023-05-02 00:25:05 -0500

    🔨 Update section divider when url is undefined

--- src/components/SectionDivider/index.tsx
+++ src/components/SectionDivider/index.tsx
@@ -7,23 +7,31 @@ const SectionDivider = ({
 }: {
   name: string;
   children: React.ReactNode;
-  url: string;
+  url?: string;
 }) => {
   return (
     <section className="flex flex-col">
-      <div className="flex flex-row justify-between max-[425px]:p-2 p-8">
-        <Link href={url}>
-          <h2 className="text-white font-bold text-2xl hover:underline cursor-pointer">
-            {name}
-          </h2>
-        </Link>
-        <div className="flex flex-row">
-          <Link
-            className="flex flex-col justify-end text-gray-400 font-bold text-lg hover:underline cursor-pointer align-bottom"
-            href={url}
-          >
-            Show all
+      <div className="flex flex-row justify-between max-[425px]:p-4 p-8">
+        {url ? (
+          <Link href={url}>
+            <h2 className="text-white font-bold text-2xl hover:underline cursor-pointer">
+              {name}
+            </h2>
           </Link>
+        ) : (
+          <h2 className="text-white font-bold text-2xl">{name}</h2>
+        )}
+        <div className="flex flex-row">
+          {url ? (
+            <Link
+              className="flex flex-col justify-end text-gray-400 font-bold text-lg hover:underline cursor-pointer align-bottom"
+              href={url}
+            >
+              Show all
+            </Link>
+          ) : (
+            <></>
+          )}
         </div>
       </div>
       <div className="flex flex-row flex-nowrap gap-6 overflow-x-scroll scrollbar-hide max-[425px]:px-2 px-8">
