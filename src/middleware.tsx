30fec774ce208309fe9b547f7c85eecddb89492e
FrigoAlex <alejo012903@hotmail.com>
2023-05-02 00:23:26 -0500

    🎨 Add format for middleware

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -8,13 +8,25 @@ export async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.SECRET });
 
   const { pathname } = req.nextUrl;
-  if (token && pathname === "/login" && (token?.accessTokenExpires && token.accessTokenExpires > Date.now())) {
+  if (
+    token &&
+    pathname === "/login" &&
+    token?.accessTokenExpires &&
+    token.accessTokenExpires > Date.now()
+  ) {
     return NextResponse.redirect(`${process.env.SPOTIFY_REDIRECT_URI}/`);
   }
-  if (pathname.match(/\/api\/auth\//) || (token?.accessTokenExpires && token.accessTokenExpires > Date.now())) {
+  if (
+    pathname.match(/\/api\/auth\//) ||
+    (token?.accessTokenExpires && token.accessTokenExpires > Date.now())
+  ) {
     return NextResponse.next();
   }
-  if (pathname !== "/login"  && ((!token) || (token?.accessTokenExpires && token.accessTokenExpires < Date.now())) ) {
+  if (
+    pathname !== "/login" &&
+    (!token ||
+      (token?.accessTokenExpires && token.accessTokenExpires < Date.now()))
+  ) {
     return NextResponse.redirect(`${process.env.SPOTIFY_REDIRECT_URI}/login`);
   }
 }
