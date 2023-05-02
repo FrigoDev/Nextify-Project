3eefb99c7ac28303470a07ae0d4b9cb64bf6a17b
FrigoAlex <alejo012903@hotmail.com>
2023-05-02 00:28:19 -0500

    ♻️ Refactor sideBar component

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,96 +1,94 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
-import { useState } from "react";
-import { BsSunFill } from "react-icons/bs";
-import { FaHome, FaSearch, FaPlusCircle, FaHeart } from "react-icons/fa";
+import { FaHome, FaSearch } from "react-icons/fa";
 import { SiLibrariesdotio } from "react-icons/si";
 import { useDispatch, useSelector } from "react-redux";
 import useSWR from "swr";
 
-import Modal from "@/components/Modal";
+import { Pages } from "@/constants/index";
+import useMediaQuery from "@/hooks/useMediaQuery";
 import { RootStates } from "@/store/models";
 import { Dispatch } from "@/store/store";
 
 const Sidebar = () => {
-  const [isOpen, setIsOpen] = useState(false);
   const { data: session } = useSession();
   const dispatch: Dispatch = useDispatch();
   const fetcher = async (key: string) => {
     await dispatch.onlinePlayList.fetchPlaylists(key);
+    await dispatch.likedAlbums.fetchLikedAlbums({
+      access_token: key,
+      offset: 0,
+    });
+    await dispatch.likedTracks.fetchLikedTracks({
+      access_token: key,
+      offset: 0,
+    });
   };
-  const { error } = useSWR(session?.accessToken, fetcher);
+
+  useSWR(session?.accessToken, fetcher);
+
   const playlist = useSelector((state: RootStates) => state.onlinePlayList);
 
-  const handleOpenModal = () => {
-    setIsOpen(true);
-  };
-  const handleCloseModal = () => {
-    setIsOpen(false);
-  };
-
-  const handleRightClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    console.log("right click");
-  };
+  const isMobile = useMediaQuery("(max-width: 450px)");
 
   return (
     <>
-      <Modal isOpen={isOpen} onClose={handleCloseModal}>
-        <div className="flex flex-col items-center space-y-4">
-          <h2 className="text-2xl font-bold">Create Playlist</h2>
-          <div className="flex items-center space-x-2">
-            <Image
-              src={session?.user?.image ?? ""}
-              width={50}
-              height={50}
-              className="rounded-full"
-              alt="user profile image"
-            />
-            <p>{session?.user?.name}</p>
+      {isMobile ? (
+        <nav className="fixed bottom-0 w-full bg-black bg-opacity-95 border-t border-gray-900 text-gray-500 pb-2 px-4 pt-4 z-10">
+          <ul className="flex justify-around">
+            <li>
+              <Link className="flex-col hover:text-white" href={Pages.HOME}>
+                <FaHome className="h-6 w-6 mx-auto" />
+                <p className="text-xs text-center">Home</p>
+              </Link>
+            </li>
+            <li>
+              <Link className="flex-col hover:text-white" href={Pages.SEARCH}>
+                <FaSearch className="h-6 w-6 mx-auto" />
+                <p className="text-xs text-center">Search</p>
+              </Link>
+            </li>
+            <li>
+              <Link className="flex-col hover:text-white" href={Pages.LIBRARY}>
+                <SiLibrariesdotio className="h-6 w-6 mx-auto" />
+                <p className="text-xs text-center">Your Library</p>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      ) : (
+        <section className="flex-shrink-0 overflow-auto h-[100vh] scrollbar-hide text-gray-500 p-5 pb-24 text-sm bg-black border-r border-gray-900 max-[450px]:hidden">
+          <div className="space-y-4">
+            <Link
+              className="flex items-center justify-center sm:justify-normal m-0 space-x-2 hover:text-white"
+              href={Pages.HOME}
+            >
+              <FaHome className="sm:h-5 sm:w-5 h-6 w-6" />
+              <p className="hidden sm:block">Home</p>
+            </Link>
+            <Link
+              className="flex items-center justify-center sm:justify-normal m-0 space-x-2 hover:text-white"
+              href={Pages.SEARCH}
+            >
+              <FaSearch className="sm:h-5 sm:w-5 h-6 w-6" />
+              <p className="hidden sm:block">Search</p>
+            </Link>
+            <Link
+              className="flex items-center justify-center sm:justify-normal m-0 space-x-2 hover:text-white"
+              href={Pages.LIBRARY}
+            >
+              <SiLibrariesdotio className="sm:h-5 sm:w-5 h-6 w-6" />
+              <p className="hidden sm:block">Your Library</p>
+            </Link>
           </div>
-        </div>
-      </Modal>
-      <section className="overflow-auto h-[100vh] scrollbar-hide text-gray-500 p-5 text-sm bg-black border-r border-gray-900 max-[400px]:hidden">
-        <div className="space-y-4">
-          <button className="flex items-center mx-auto md:m-0 space-x-2 hover:text-white">
-            <FaHome className="h-5 w-5" />
-            <p className="hidden md:inline-flex">Home</p>
-          </button>
-          <button className="flex items-center mx-auto md:m-0 space-x-2 hover:text-white">
-            <FaSearch className="h-5 w-5" />
-            <p className="hidden md:inline-flex">Search</p>
-          </button>
-          <button className="flex items-center mx-auto md:m-0 space-x-2 hover:text-white">
-            <SiLibrariesdotio className="h-5 w-5" />
-            <p className="hidden md:inline-flex">Your Library</p>
-          </button>
-          <hr className="border-t-[0.1]px border-gray-900" />
-          <button
-            onClick={handleOpenModal}
-            className="flex items-center mx-auto md:m-0 space-x-2 hover:text-white"
-          >
-            <FaPlusCircle className="h-5 w-5" />
-            <p className="hidden md:inline-flex">Create Playlist</p>
-          </button>
-          <button className="flex items-center mx-auto md:m-0 space-x-2 hover:text-white">
-            <FaHeart className="h-5 w-5" />
-            <p className="hidden md:inline-flex">Liked Songs</p>
-          </button>
-          <button className="flex items-center mx-auto md:m-0 space-x-2 hover:text-white">
-            <BsSunFill className="h-5 w-5" />
-            <p className="hidden md:inline-flex">Made For You</p>
-          </button>
-        </div>
-        <div className="space-y-4">
-          <hr className="border-t-[0.1]px border-gray-900 mt-4" />
-          <div className="">
+          <div className="space-y-4">
+            <hr className="border-t-[0.1]px border-gray-900 mt-4" />
             {playlist.map((item) => (
               <Link
                 href={`/playlists/${item.id}`}
                 key={item.id}
                 className="flex items-center space-x-2 hover:text-white mb-2"
-                onContextMenu={handleRightClick}
               >
                 <Image
                   src={
@@ -100,21 +98,16 @@ const Sidebar = () => {
                   alt={item.name}
                   width={50}
                   height={50}
-                  className="h-[50px] object-cover rounded-sm"
+                  className="h-[50px] w-[50px] mx-auto object-cover rounded-sm"
                 />
-
-                <div
-                  className={
-                    "overflow-hidden whitespace-nowrap overflow-ellipsisw-[140px] hidden md:inline-flex md:w-[200px] lg:w-[250px]"
-                  }
-                >
-                  <p className="text-sm">{item.name}</p>
-                </div>
+                <p className="text-sm truncate hidden w-24 sm:mx-0 sm:block md:w-36">
+                  {item.name}
+                </p>
               </Link>
             ))}
           </div>
-        </div>
-      </section>
+        </section>
+      )}
     </>
   );
 };
