fc60b4199fca7c1d58fa31dc636ddcc498fc3213
FrigoAlex <alejo012903@hotmail.com>
2023-05-02 00:18:09 -0500

    ✨ Add 404 page

---
+++ src/pages/404/index.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Custom404 = () => {
+  return (
+    <div className="flex flex-col justify-center align-middle w-full items-center">
+      <Image
+        priority
+        width={200}
+        height={200}
+        src="/assets/images/spotifyLogo.png"
+        alt="Spotify logo"
+        className="m-5"
+      />
+      <h1 className="text-white text-4xl my-2 font-bold">Page Not Found</h1>
+      <p className="text-white mb-6 text-lg">
+        We cannot find the page you are looking for.
+      </p>
+      <Link
+        className="bg-[#1dd661] text-white px-6 py-3 rounded-full font-semibold hover:bg-[#0f6f32] duration-700 ease-in-out"
+        href="/"
+      >
+        Home
+      </Link>
+    </div>
+  );
+};
+export default Custom404;
