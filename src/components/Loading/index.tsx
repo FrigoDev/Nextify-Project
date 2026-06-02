75e786d2d5a0466f930e4d50249ae4ef6142b4f8
FrigoDev <alejo012903@gmail.com>
2026-06-02 01:38:09 -0500

    ✨ Show a loading bar on route changes

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,6 +1,20 @@
+import { useSelector } from "react-redux";
+
 import useLoading from "@/hooks/useLoading";
+import { RootState } from "@/store/store";
 
 export default function Loading({ children }: { children: React.ReactNode }) {
   useLoading();
-  return <>{children}</>;
+  const loading = useSelector((state: RootState) => state.loading);
+
+  return (
+    <>
+      {loading && (
+        <div className="fixed top-0 left-0 z-50 h-1 w-full overflow-hidden">
+          <div className="h-full w-1/3 rounded-r-full bg-green-500 animate-[loading-bar_0.9s_linear_infinite]" />
+        </div>
+      )}
+      {children}
+    </>
+  );
 }
--- src/hooks/useLoading.tsx
+++ src/hooks/useLoading.tsx
@@ -1,18 +1,21 @@
 import { Router } from "next/router";
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
-import { Dispatch, RootState } from "@/store/store";
+import { Dispatch } from "@/store/store";
 
 export default function useLoading() {
   const dispatch: Dispatch = useDispatch();
-  const loading = useSelector((state: RootState) => state.loading);
   useEffect(() => {
-    Router.events.on("routeChangeStart", () => {
-      return !loading && dispatch.loading.setLoading(true);
-    });
-    Router.events.on("routeChangeComplete", () => {
-      return loading && dispatch.loading.setLoading(false);
-    });
-  }, [loading, dispatch]);
+    const start = () => dispatch.loading.setLoading(true);
+    const end = () => dispatch.loading.setLoading(false);
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+      Router.events.off("routeChangeError", end);
+    };
+  }, [dispatch]);
 }
